4e2483eb3fe78c65b2015074612f1b60ffa2cc20
Danilo Pejovic <pejovicdanilo@outlook.com>
2023-06-05 01:30:01 +0200

    Failed attempt at custm header

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,50 @@
-const CustomHeader = ({ navigation, loggedIn }) => {
-    return (
-      <Header>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
-            <Text>Username</Text>
-          </TouchableOpacity>
-          {loggedIn ? (
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-              <Text>Log Out</Text>
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-              <Text>Log In</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-      </Header>
-    );
+import React from 'react';
+import { HeaderBackButton } from '@react-navigation/elements';
+import { useNavigation } from '@react-navigation/native';
+import { View, TouchableOpacity, Text } from 'react-native';
+
+
+// TO DO: Try to make more CUSTOM functional header in the future
+
+function Header(props) {
+  const navigation = useNavigation();
+
+  const handleGoBack = () => {
+    navigation.goBack();
   };
-  
-  export default CustomHeader;
+
+  return (
+    <View style={styles.container}>
+      {props.scene?.route?.name !== 'Home' && (
+        <TouchableOpacity onPress={handleGoBack} style={styles.backButton}>
+          <HeaderBackButton labelVisible={false} />
+        </TouchableOpacity>
+      )}
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>{props.scene?.descriptor?.options?.title}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = {
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    height: 56,
+    backgroundColor: '#f0f0f0',
+  },
+  backButton: {
+    marginRight: 8,
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+};
+
+export default Header;
